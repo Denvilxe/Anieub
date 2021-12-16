0fc8a516a8b4cbdb016525f34a9974cb2cbd1fd1
Denvilxe <90274962+Denvilxe@users.noreply.github.com>
2021-12-17 00:34:04 +0530

    Update __init__.py

--- Aniebot/__init__.py
+++ Aniebot/__init__.py
@@ -1,8 +1,8 @@
-# Copyright (C) 2020-2021 by okay-retard@Github, < https://github.com/okay-retard >.
+# Copyright (C) 2020-2021 by Xdenvil@Github, < https://github.com/xdenvil >.
 #
-# This file is part of < https://github.com/okay-retard/ZectUserBot > project,
+# This file is part of < https://github.com/Anieteam/Aniebots> project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/okay-retard/ZectUserBot/blob/master/LICENSE >
+# Please see < https://github.com/Anieteam/Aniebots/blob/master/LICENSE >
 #
 # All rights reserved.
 
